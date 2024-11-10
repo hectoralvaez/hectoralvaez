a085bc399afc9dbc12132d6538e1c4688b18fff3
Héctor <hectoralvaez@gmail.com>
2024-11-10 12:50:54 +0100

    JS: STATS: Startup stats.

---
+++ src/data/generalTagsData.js
@@ -0,0 +1,10 @@
+export const generalTagsData = [
+    "HTML",
+    "SASS",
+    "BEMIT",
+    "Accelerated Mobile Pages",
+    "Core Web Vitals",
+    "Jira",
+    "Webpack",
+    "Symfony",
+];
---
+++ src/data/skillsData.js
@@ -0,0 +1,9 @@
+export const skillsData = [
+    { skill: "HTML", value: 100 },
+    { skill: "SASS", value: 90 },
+    { skill: "REACT", value: 75 },
+    { skill: "SYMFONY", value: 60 },
+    { skill: "FIGMA", value: 75 },
+    { skill: "GIT", value: 80 },
+    { skill: "JIRA", value: 90 },
+];
