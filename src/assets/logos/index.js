9ab70cd3c29916a8426bd74cde901e2d094e30bb
Héctor <hectoralvaez@gmail.com>
2024-11-10 13:19:10 +0100

    SVG: LOGOS: Add new logos.

---
+++ src/assets/logos/index.js
@@ -0,0 +1,13 @@
+// Importa cada SVG como un componente de React
+import { ReactComponent as AtlassianLogo } from './atlassian.svg';
+import { ReactComponent as Css3Logo } from './css3.svg';
+import { ReactComponent as FigmaLogo } from './figma.svg';
+import { ReactComponent as GitLogo } from './git.svg';
+import { ReactComponent as Html5Logo } from './html5.svg';
+import { ReactComponent as ReactLogo } from './react.svg';
+import { ReactComponent as SassLogo } from './sass.svg';
+import { ReactComponent as SymfonyLogo } from './symfony.svg';
+
+
+// Exporta todos los íconos juntos
+export { AtlassianLogo, Css3Logo, FigmaLogo, GitLogo, Html5Logo, ReactLogo, SassLogo, SymfonyLogo };
