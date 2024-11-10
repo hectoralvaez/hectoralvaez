a7fa391efb06783ed4672768e002852a6f797009
Héctor <hectoralvaez@gmail.com>
2024-11-10 13:19:56 +0100

    JSX: APP: Startup layout.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { Tags } from "./components/Tags"
+import { generalTagsData } from "./data/generalTagsData"
+import MySkills from "./stats/MySkills"
+import { AtlassianLogo, Css3Logo, FigmaLogo, GitLogo, Html5Logo, ReactLogo, SassLogo, SymfonyLogo } from './assets/logos';
+
 
 function App() {
 
   return (
     <>
-      hectoralvaez
+      
+      <h1>Héctor Penalva Peláez</h1>
+      <p>Lorem ipsum <a href="#">dolor sit amet consectetur adipisicing elit</a>. Culpa, nam nemo facilis incidunt excepturi ipsum, ab laborum dignissimos veritatis minima deleniti repellat. Placeat architecto quis iusto quasi tempora officiis minima?</p>
+      <MySkills />
+      <Tags items={ generalTagsData }  />
+      <Tags items={["fasdf", "sesdfasd"]}  />
+      <AtlassianLogo />
     </>
   )
 }
